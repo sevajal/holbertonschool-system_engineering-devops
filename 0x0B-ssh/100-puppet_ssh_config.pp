75efec2b4151f808fab56786e35f8d7e0d9365fd
sevajal <sevajal@yahoo.com>
2022-08-23 10:06:04 -0500

    I changed the code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Manifest to connect to a server without typing a password.
 exec { 'SSH client configuration':
-  command => 'ssh-copy-id -i ~/.ssh/school ubuntu@54.160.169.188',
+  command => [ 'eval "$(ssh-agent)"', 'ssh-add' ],
   path    => [ '/bin', '/sbin' ],
 }
