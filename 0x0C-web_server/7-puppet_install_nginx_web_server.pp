f09b8ac8a6ed627cbb30300f8f1f09b09d4b7dc2
sevajal <sevajal@yahoo.com>
2022-08-25 16:22:34 -0500

    Task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# Manifest to install Nginx web server.
+package { 'nginx':
+  ensure => 'installed',
+}
+
+service { 'nginx':
+  ensure => 'running',
+  require => 'package['nginx']',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+file_line { 'redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'rewrite ^/redirect_me https://www.mocionesdevida.com permanent;',
+  after  => 'server_name _;',
+}
