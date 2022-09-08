4b73761906cfc6c56502404dbe7c0e269a4d7bfa
sevajal <sevajal@yahoo.com>
2022-09-08 12:11:02 -0500

    I did the task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script to export data in the CSV format."""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    f = open(f'{sys.argv[1]}.csv', 'w', encoding='UTF8', newline='')
+    writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+    id = int(sys.argv[1])
+    r_user = requests.get(f'https://jsonplaceholder.typicode.com/users/')
+    r_user = r_user.json()
+    for user in r_user:
+        if user['id'] == id:
+            username = user['username']
+    r_todos = requests.get(f'https://jsonplaceholder.typicode.com/todos/')
+    r_todos = r_todos.json()
+    for task in r_todos:
+        if task['userId'] == id:
+            status = task['completed']
+            task = task['title']
+            writer.writerow([id, username, status, task])
+    f.close()
