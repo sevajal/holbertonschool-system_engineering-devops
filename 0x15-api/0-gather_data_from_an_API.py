6d154ff1a61af6a297820147cc20b0a4b0adf8cc
sevajal <sevajal@yahoo.com>
2022-09-09 09:46:47 -0500

    I changed the printing format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,12 +6,12 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    r_user = requests.get(f'https://jsonplaceholder.typicode.com/users/')
+    r_user = requests.get('https://jsonplaceholder.typicode.com/users/')
     r_user = r_user.json()
     for user in r_user:
         if user['id'] == int(sys.argv[1]):
             name = user['name']
-    r_todos = requests.get(f'https://jsonplaceholder.typicode.com/todos/')
+    r_todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
     r_todos = r_todos.json()
     total_tasks = 0
     completed = 0
@@ -22,6 +22,7 @@ if __name__ == "__main__":
             if task['completed'] is True:
                 completed += 1
                 tasks_completed.append(task['title'])
-    print(f"Employee {name} is done with tasks({completed}/{total_tasks}):")
+    print("Employee {} is done with tasks({}/{}):".format(
+        name, completed, total_tasks))
     for task in tasks_completed:
-        print(f"\t {task}")
+        print("\t {}".format(task))
